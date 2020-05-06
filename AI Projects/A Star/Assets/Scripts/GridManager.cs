a9a237f5b4fec2f2d5aca4b73fb576c50e521713
anukulu <anukulparajuli9@gmail.com>
2020-05-06 09:58:17 +0545

    committed

---
+++ AI Projects/A Star/Assets/Scripts/GridManager.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public LayerMask unwalkableMask;
+    public Vector2 gridWorldSize;
+    public float nodeRadius;
+    Node[,] grid;
+
+    float nodeDiameter;
+    int gridSizeX, gridSizeY;
+    void Start()
+    {
+        nodeDiameter = nodeRadius * 2;
+        gridSizeX = Mathf.RoundToInt(gridWorldSize.x/nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridWorldSize.y/nodeDiameter);
+        CreateGrid();
+    }
+
+    void CreateGrid()
+    {
+        grid = new Node[gridSizeX, gridSizeY];
+        Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x/2 - Vector3.forward * gridWorldSize.y/2;
+
+        for (int x = 0 ; x < gridWorldSize.x; x++)
+        {   
+            for (int y = 0 ; y < gridWorldSize.y; y++)
+            {
+                Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
+                bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask));
+                grid[x,y] = new Node(walkable, worldPoint, x, y);
+            }
+        }
+
+    }
+
+    public Node NodeFromWorldPoint(Vector3 worldPoint)
+    {
+        float percentX = (worldPoint.x + gridWorldSize.x/2) / gridWorldSize.x;
+        float percentY = (worldPoint.z + gridWorldSize.y/2) /gridWorldSize.y;
+        percentX = Mathf.Clamp01(percentX);
+        percentY = Mathf.Clamp01(percentY);
+
+        int x = Mathf.FloorToInt(Mathf.Min(gridSizeX * percentX, gridSizeX - 1));
+        int y = Mathf.FloorToInt(Mathf.Min(gridSizeY * percentY, gridSizeY - 1));
+        return grid[x,y];
+    }
+
+    public List<Node> path;
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
+
+        if(grid != null)
+        {
+            foreach(Node n in grid)
+            {
+                Gizmos.color = (n.walkable)? Color.white : Color.red;
+                if(path != null)
+                {
+                    if(path.Contains(n))
+                        Gizmos.color = Color.black;
+                }
+                Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter -0.1f));
+            }
+        }
+    }
+
+    public List<Node> GetNeighbours(Node node)
+    {
+        List<Node> neighbours = new List<Node>();
+        for (int x = -1; x <= 1; x ++)
+        {
+            for (int y = -1; y <=1 ; y ++)
+            {
+                if(x == 0 && y ==0)
+                    continue;
+                
+                int checkX = node.gridX + x;
+                int checkY = node.gridY + y;
+
+                if(checkX >= 0 && checkX < gridSizeX && checkY >=0 && checkY < gridSizeY)
+                {
+                    neighbours.Add(grid[checkX, checkY]);
+                }
+            }
+        }
+        return neighbours;
+
+    }
+}
---
+++ AI Projects/A Star/Assets/Scripts/Heap.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Heap<T> where T : IHeapItem<T> 
+{
+    T[] items;
+    int currentItemCount;
+
+    public Heap(int maxHeapSize)
+    {
+        items = new T[maxHeapSize];
+    }
+
+    public void Add(T item)
+    {
+        item.heapIndex = currentItemCount;
+        items[currentItemCount] = item;
+        SortUp(item);
+        currentItemCount ++;
+    }
+
+    public T RemoveFirstItem()
+    {
+        T firstItem = items[0];
+        currentItemCount --;
+        items[0] = items[currentItemCount];
+        items[0].heapIndex = 0;
+        SortDown(items[0]);
+        return firstItem;
+    }
+
+    void SortDown(T item)
+    {
+        while(true)
+        {
+            int childIndexLeft = (item.heapIndex * 2) + 1;
+            int childIndexRight = (item.heapIndex * 2) + 2;
+            int swapIndex = 0;
+
+            if(childIndexLeft < currentItemCount)
+            {
+                swapIndex = childIndexLeft;
+                if(childIndexRight < currentItemCount)
+                {
+                    if(items[childIndexLeft].CompareTo(items[childIndexRight]) < 0)
+                    {
+                        swapIndex = childIndexRight;
+                    }
+                }
+                if(item.CompareTo(items[swapIndex]) < 0)
+                {
+                    Swap(item, items[swapIndex]);
+                }
+                else{return;}
+            }
+            else{return;}
+            
+        }
+    }
+
+    public bool Contains(T item)
+    {
+        return Equals(items[item.heapIndex], item);
+    }
+
+    public int Count
+    {
+        get{return currentItemCount;}
+    }
+
+    public void Update(T item)
+    {
+        SortUp(item);
+    }
+
+    void SortUp(T item)
+    {
+        int parentIndex = (int)(item.heapIndex - 1)/2;
+
+        while(true)
+        {
+            T parentItem = items[parentIndex];
+            if(item.CompareTo(parentItem) > 0)
+            {
+                Swap(item, parentItem);
+            }
+            else{break;}
+
+            parentIndex = (int)(item.heapIndex - 1)/2;
+        }
+    }
+
+    void Swap(T itemA, T itemB)
+    {
+        items[itemA.heapIndex] = itemB;
+        items[itemB.heapIndex] = itemA;
+        int itemAIndex = itemA.heapIndex;
+        itemA.heapIndex = itemB.heapIndex;
+        itemB.heapIndex = itemAIndex;
+    }
+}
+
+public interface IHeapItem<T> : IComparable<T> // this is a virtual class
+{
+    int heapIndex{get; set;}
+}
