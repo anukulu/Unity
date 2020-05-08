af282bca7d0a6b13528b63f1b64d65da6183ed84
anukulu <anukulparajuli9@gmail.com>
2020-05-08 09:47:10 +0545

    commited new project

---
+++ Gravity Simulation/Assets/Scripts/CameraMMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Camera-Control/Mouse Orbit with zoom")]
+public class CameraMMovement : MonoBehaviour
+{
+    public Transform target;
+    public float RotateAmount = 15f;
+    void LateUpdate()
+    {
+        OrbitCamera();
+    }
+
+    public void OrbitCamera()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 targetPos = target.position; //this is the center of the scene, you can use any point here
+            float y_rotate = Input.GetAxis("Mouse X") * RotateAmount;
+            float x_rotate = Input.GetAxis("Mouse Y") * RotateAmount;
+            OrbitCamera(targetPos, y_rotate, x_rotate);
+        }
+    }
+
+    public void OrbitCamera(Vector3 targetPos, float y_rotate, float x_rotate)
+    {
+        Vector3 angles = transform.eulerAngles;
+        angles.z = 0;
+        transform.eulerAngles = angles;
+        transform.RotateAround(targetPos, Vector3.up, y_rotate);
+        transform.RotateAround(targetPos, Vector3.left, x_rotate);
+
+        transform.LookAt(target);
+    }
+}
---
+++ Gravity Simulation/Assets/Scripts/Orbitters.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Orbitters : MonoBehaviour
+{
+    public InputField iPlanetSpeed;
+    public InputField iSunSize;
+    public InputField iDistanceFromSun;
+    public InputField iPlanetNumber;
+
+    private int planetCount = 5;
+    public int maxRadius = 50;
+    private float sunSize = 1;
+    public GameObject[] planets;    //planets == spheres
+    public Material[] mats;
+    public Material trailMaterial;
+    private float forwardMovementSpeed;
+    private bool isSetNumbers = false;
+
+    public void InitSettings()
+    {
+        forwardMovementSpeed = float.Parse(iPlanetSpeed.text);
+        sunSize = float.Parse(iSunSize.text);
+        planetCount = int.Parse(iPlanetNumber.text);
+        //distanceFromSun = PlayerPrefs.GetInt("planetSpeed");
+        PlayerPrefs.SetInt("hasSetNumbers", 1);
+        PlayerPrefs.Save();
+        GameObject.Find("Canvas").SetActive(false);
+        GameObject.Find("UIBG").SetActive(false);
+        GameObject.Find("EventSystem").SetActive(false);
+        isSetNumbers = true;
+        planets = new GameObject[planetCount];
+        CreatePlanets();
+    }
+
+
+    public void CreatePlanets()
+    {
+        print("here");
+        transform.localScale *= sunSize; 
+        var planetToCopy = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        Rigidbody rb = planetToCopy.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+        print(planetCount);
+        for(int i = 0; i < planetCount; i++)
+        {
+            GameObject pl = Instantiate(planetToCopy);
+            pl.transform.position = new Vector3(Random.Range(-maxRadius, maxRadius), Random.Range(-10, 10), Random.Range(-maxRadius, maxRadius));
+            pl.transform.localScale *= Random.Range(0.5f, 1);
+            pl.GetComponent<Renderer>().material = mats[Random.Range(0, mats.Length)];
+            TrailRenderer tr = pl.AddComponent<TrailRenderer>();
+            tr.time = 1.0f;
+            tr.startWidth = 0.1f;
+            tr.endWidth = 0f;
+            tr.material = trailMaterial;
+            tr.startColor = new Color(1, 1, 0, 0.1f);
+            tr.endColor = new Color(0, 0, 0, 0);
+            Rigidbody rbTemp = GetComponent<Rigidbody>();
+            rbTemp.velocity = new Vector3(rbTemp.velocity.x, rbTemp.velocity.y, forwardMovementSpeed);
+            planets[i] = pl;
+        }        
+        Destroy(planetToCopy);
+    }
+
+    void Update()
+    {
+        if(isSetNumbers)
+        {
+            foreach (GameObject p in planets)
+            {
+                Vector3 difference = this.transform.position - p.transform.position;
+                float distance = difference.magnitude;
+                Vector3 gravityDirection = difference.normalized;
+                float gravity = 6.7f * ((this.transform.localScale.x * p.transform.localScale.x * 80) / Mathf.Pow(distance, 2));
+                Vector3 gravityVector = gravityDirection * gravity;
+                p.transform.GetComponent<Rigidbody>().AddForce(p.transform.forward, ForceMode.Acceleration);
+                p.transform.GetComponent<Rigidbody>().AddForce(gravityVector, ForceMode.Acceleration);
+            }
+        }
+    }
+
+}
